8706ed0491294430f78cebbff401df1d6ac5efef
Amir thapa magar <amirthapa2019@gmail.com>
2023-02-24 11:45:33 +0545

    feat:will finish 9.12

--- src/routes/patients.ts
+++ src/routes/patients.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import patientsList from "../services/patients";
+import { v1 as uuid } from "uuid";
 
 const router = express.Router();
 
 router.get("/", (_req, res) => {
   return res.send(patientsList.getAllPatientExcludingSsn());
+});
+
+router.post("/", (req, res) => {
+  return res.send({ ...req.body, id: uuid() });
 });
 
 export default router;
